b1499d5e57669799e8f87b5b9bce4e6af1d61508
hanning1 <hanning1@msu.edu>
2020-03-25 13:19:42 +1100

    Added nodejs and alterred account page

---
+++ build/precache-manifest.81856b4be41588ddab4ca03144394eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aba96d20b107e356ef15dab8f715b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de2eeb8621073fa97ea5",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ed6e51da6ffb08384f35",
+    "url": "/static/css/main.4e99c3b3.chunk.css"
+  },
+  {
+    "revision": "de2eeb8621073fa97ea5",
+    "url": "/static/js/2.8dfcf542.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.8dfcf542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6e51da6ffb08384f35",
+    "url": "/static/js/main.3ff2145b.chunk.js"
+  },
+  {
+    "revision": "decedf9774cf44d7d3dd",
+    "url": "/static/js/runtime-main.b16a713c.js"
+  }
+]);
--- frontend/src/components/pages/Account.js
+++ frontend/src/components/pages/Account.js
@@ -9,11 +9,40 @@ import {
 import SideBar from "./SideBar";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-import "../../styles/Account.css";
+import "../../styles/Account.scss";
 
 class Account extends Component {
-	state = {};
+	state = {
+		Name: "Username",
+		Authority: "Admin",
+		Address: "1234 Swanston St",
+		City: "Melbourne",
+		State: "Victoria",
+		Postcode: "3000",
+		Mobile: "XXXX",
+		Telephone: "XXXX",
+		Email: "XXX@XXX",
+		Fax: "XXX",
+		inEditMode: false
+	};
+
+	handleProfileButton = () => {
+		// event.preventDefault();
+		this.setState({ inEditMode: true });
+	};
+
+	handleDetailSubmit = event => {
+		event.preventDefault();
+		this.setState({ inEditMode: false });
+	};
+
+	handleNameChange = e => {
+		this.setState({ Name: e.target.value });
+		console.log("change to: ", this.state.Name);
+	};
+
 	render() {
 		return (
 			<div>
@@ -39,6 +68,7 @@ class Account extends Component {
 								variant="outline-info"
 								className="profileChange"
 								size="lg"
+								onClick={this.handleProfileButton}
 							>
 								Change profile information
 							</Button>
@@ -46,16 +76,168 @@ class Account extends Component {
 					</div>
 					<h1 className="title">My Detail</h1>
 					<div className="account_detail">
-						<h1>Name: Username</h1>
-						<h1>Authority: Admin</h1>
-						<h1>Address: XXX Street</h1>
-						<h1>City: Melbourne</h1>
-						<h1>State: VIC</h1>
-						<h1>Postcode: 3130</h1>
+						<Form>
+							<Form.Row>
+								<Form.Group
+									className="formName"
+									controlId="formGridName"
+								>
+									<Form.Label as={Form.col} size="lg" lg={2}>
+										Name
+									</Form.Label>
+									<Form.Control
+										type="Name"
+										as={Form.col}
+										placeholder={this.state.Name}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+										onChange={this.handleNameChange}
+									/>
+								</Form.Group>
+							</Form.Row>
+							<Form.Row>
+								<Form.Group
+									as={Form.col}
+									controlId="formGridAddress1"
+								>
+									<Form.Label size="lg">Address</Form.Label>
+									<Form.Control
+										placeholder={this.state.Address}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									/>
+								</Form.Group>
+
+								<Form.Group
+									as={Form.col}
+									controlId="formGridCity"
+								>
+									<Form.Label size="lg">City</Form.Label>
+									<Form.Control
+										placeholder={this.state.City}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									/>
+								</Form.Group>
+							</Form.Row>
+							<Form.Row>
+								<Form.Group
+									as={Form.col}
+									controlId="formGridState"
+								>
+									<Form.Label size="lg">State</Form.Label>
+									<Form.Control
+										as="select"
+										value="Choose..."
+										placeholder={this.state.State}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									>
+										<option>Victoria</option>
+										<option>New South Wales</option>
+										<option>Tasmania</option>
+										<option>Queensland</option>
+										<option>Western Australia</option>
+										<option>South Australia</option>
+									</Form.Control>
+								</Form.Group>
+
+								<Form.Group
+									as={Form.col}
+									controlId="formGridPostcode"
+								>
+									<Form.Label size="lg">Postcode</Form.Label>
+									<Form.Control
+										placeholder={this.state.Postcode}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									/>
+								</Form.Group>
+							</Form.Row>
+							{this.state.inEditMode ? (
+								<Button
+									variant="primary"
+									// type="submit"
+									className="account_detail_btn"
+									onClick={this.handleDetailSubmit}
+									size="lg"
+								>
+									Submit
+								</Button>
+							) : null}
+						</Form>
 					</div>
 					<h1 className="title">Contact</h1>
 					<div className="account_contact">
-						<h1>Block for contact</h1>
+						<Form>
+							<Form.Row>
+								<Form.Group
+									as={Form.col}
+									controlId="formGridMobile"
+									className="contact_item"
+								>
+									<Form.Label size="lg">
+										Mobile Phone
+									</Form.Label>
+									<Form.Control
+										placeholder={this.state.Mobile}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									/>
+								</Form.Group>
+
+								<Form.Group
+									as={Form.col}
+									controlId="formGridTelephone"
+									className="contact_item"
+								>
+									<Form.Label size="lg">Telephone</Form.Label>
+									<Form.Control
+										placeholder={this.state.Telephone}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									/>
+								</Form.Group>
+							</Form.Row>
+							<Form.Row>
+								<Form.Group
+									as={Form.col}
+									controlId="formGridEmail"
+									className="contact_item"
+								>
+									<Form.Label size="lg">Email</Form.Label>
+									<Form.Control
+										placeholder={this.state.Email}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									/>
+								</Form.Group>
+
+								<Form.Group
+									as={Form.col}
+									controlId="formGridFax"
+									className="contact_item"
+								>
+									<Form.Label size="lg">Fax</Form.Label>
+									<Form.Control
+										placeholder={this.state.Fax}
+										readOnly={!this.state.inEditMode}
+										size="lg"
+									/>
+								</Form.Group>
+							</Form.Row>
+							{this.state.inEditMode ? (
+								<Button
+									variant="primary"
+									// type="submit"
+									className="account_contact_btn"
+									onClick={this.handleContactSubmit}
+									size="lg"
+								>
+									Submit
+								</Button>
+							) : null}
+						</Form>
 					</div>
 				</div>
 			</div>
